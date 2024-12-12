7d2a89747899fc92878c017aec01dc506e901baa
Kadek Indra Purnama Mertayana <indrakadek027@gmail.com>
2024-12-12 23:27:08 +0800

    Add Login Page

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,13 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
+
 Route::get('/', function () {
-    return view('welcome');
+    return view('login');
 });
 
 Route::resource('/products', \App\Http\Controllers\ProductController::class);
+
+Route::get('/login',   [AuthController::class, 'login'])->name('login');
